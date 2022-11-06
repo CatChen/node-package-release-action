e4a8faa4d7741cfb3713cb7583dbfc2dc15b9ee5
Cat Chen <catchen@catchen.me>
2022-11-06 00:29:50 -0700

    Implemented reading version from package.json (#4)

---
+++ lib/getPackageVersion.d.ts
@@ -0,0 +1,2 @@
+export declare const DEFAULT_WORKING_DIRECTORY: string;
+export declare function getPackageVersion(directory?: string): Promise<string>;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { notice, error } from "@actions/core";
 import { getLastGitTag } from "./getLastGitTag";
+import { getPackageVersion } from "./getPackageVersion";
 
 async function run(): Promise<void> {
   const lastGitTag = await getLastGitTag();
   notice(`Last git tag: ${lastGitTag}`);
+  const packageVersion = await getPackageVersion();
+  notice(`package.json version: ${packageVersion}`);
 }
 
 async function cleanup(): Promise<void> {
