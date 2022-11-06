039433fbc2e2e748c6278c32432aeb5c0654ac3f
Cat Chen <catchen@catchen.me>
2022-11-06 10:40:33 -0800

    Removed unused files

--- src/pushBranch.ts
+++
@@ -1,8 +0,0 @@
-import { getExecOutput } from "@actions/exec";
-
-export async function pushBranch() {
-  const gitPushOutput = await getExecOutput("git", ["push", "--follow-tags"]);
-  if (gitPushOutput.exitCode !== 0) {
-    throw new Error(gitPushOutput.stderr);
-  }
-}
