b90162635f5467b09eb6a558d404b40d149f6972
Cat Chen <catchen@catchen.me>
2022-11-05 22:50:56 -0700

    Removed logging

--- src/getLastGitTag.ts
+++ src/getLastGitTag.ts
@@ -6,8 +6,6 @@ export async function getLastGitTag() {
     "--tags",
     "--max-count=1",
   ]);
-  console.log(lastTaggedCommitOutput);
-  console.log(JSON.stringify(lastTaggedCommitOutput));
   if (lastTaggedCommitOutput.exitCode !== 0) {
     throw new Error(lastTaggedCommitOutput.stderr);
   }
