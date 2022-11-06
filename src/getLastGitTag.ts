10301189b1ee7a034fcbdec732eb4984619acdb0
Cat Chen <catchen@catchen.me>
2022-11-05 22:37:32 -0700

    Implemented getting last git tag

---
+++ src/getLastGitTag.ts
@@ -0,0 +1,23 @@
+import { getExecOutput } from "@actions/exec";
+
+export async function getLastGitTag() {
+  const lastTaggedCommitOutput = await getExecOutput("git", [
+    "rev-list",
+    "--tags",
+    "--max-count=1",
+  ]);
+  if (lastTaggedCommitOutput.exitCode !== 0) {
+    throw new Error(lastTaggedCommitOutput.stderr);
+  }
+  const lastTaggedCommit = lastTaggedCommitOutput.stdout;
+  const lastTagOutput = await getExecOutput("git", [
+    "describe",
+    "--tags",
+    lastTaggedCommit,
+  ]);
+  if (lastTaggedCommitOutput.exitCode !== 0) {
+    throw new Error(lastTagOutput.stderr);
+  }
+  const lastTag = lastTagOutput.stdout;
+  return lastTag;
+}
