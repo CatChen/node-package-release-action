c378ac8c99a084c4f9c87b618c79259629b0d85d
Cat Chen <catchen@catchen.me>
2022-11-07 16:58:01 -0800

    Calling git tag instead of npm version if package.json does not exist (#46)

--- src/setVersion.ts
+++ src/setVersion.ts
@@ -1,8 +1,21 @@
+import { existsSync } from "node:fs";
+import { resolve } from "node:path";
 import { getExecOutput } from "@actions/exec";
 
-export async function setVersion(version: string) {
-  const npmVersionOutput = await getExecOutput("npm", ["version", version]);
-  if (npmVersionOutput.exitCode !== 0) {
-    throw new Error(npmVersionOutput.stderr);
+export const DEFAULT_WORKING_DIRECTORY = process.cwd();
+
+export async function setVersion(version: string, directory = "./") {
+  const absoluteDirectory = resolve(DEFAULT_WORKING_DIRECTORY, directory);
+  const packageJsonPath = resolve(absoluteDirectory, "package.json");
+  if (existsSync(packageJsonPath)) {
+    const npmVersionOutput = await getExecOutput("npm", ["version", version]);
+    if (npmVersionOutput.exitCode !== 0) {
+      throw new Error(npmVersionOutput.stderr);
+    }
+  } else {
+    const gitTagOutput = await getExecOutput("git", ["tag", version]);
+    if (gitTagOutput.exitCode !== 0) {
+      throw new Error(gitTagOutput.stderr);
+    }
   }
 }
