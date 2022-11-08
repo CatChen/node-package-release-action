6e40fd7966c38f3e9b947b5297f327f7a911dff4
Cat Chen <catchen@catchen.me>
2022-11-08 14:15:29 -0800

    Avoid creating tag v0 and v0.0 (#57)
    * Avoid creating tag v0 and v0.0
    
    * Added notice printout

--- src/updateTags.ts
+++ src/updateTags.ts
@@ -1,4 +1,4 @@
-import { ExitCode, getBooleanInput, warning } from "@actions/core";
+import { ExitCode, getBooleanInput, notice, warning } from "@actions/core";
 import { getExecOutput } from "@actions/exec";
 import { parse } from "semver";
 
@@ -19,21 +19,32 @@ export async function updateTags(version: string) {
     );
   }
 
-  const gitTagMajorOutput = await getExecOutput("git", [
-    "tag",
-    "-f",
-    `v${semver.major}`,
-  ]);
-  if (gitTagMajorOutput.exitCode !== 0) {
-    throw new Error(gitTagMajorOutput.stderr);
+  if (semver.major > 0) {
+    const gitTagMajorOutput = await getExecOutput("git", [
+      "tag",
+      "-f",
+      `v${semver.major}`,
+    ]);
+    if (gitTagMajorOutput.exitCode !== 0) {
+      throw new Error(gitTagMajorOutput.stderr);
+    }
+    notice(`Tag updated: v${semver.major}`);
+  } else {
+    warning(`Tag v0 is not allowed so it's not updated`);
   }
-  const gitTagMinorOutput = await getExecOutput("git", [
-    "tag",
-    "-f",
-    `v${semver.major}.${semver.minor}`,
-  ]);
-  if (gitTagMinorOutput.exitCode !== 0) {
-    throw new Error(gitTagMinorOutput.stderr);
+
+  if (semver.major > 0 || semver.minor > 0) {
+    const gitTagMinorOutput = await getExecOutput("git", [
+      "tag",
+      "-f",
+      `v${semver.major}.${semver.minor}`,
+    ]);
+    if (gitTagMinorOutput.exitCode !== 0) {
+      throw new Error(gitTagMinorOutput.stderr);
+    }
+    notice(`Tag updated: v${semver.major}.${semver.minor}`);
+  } else {
+    warning(`Tag v0.0 is not allowed so it's not updated`);
   }
 
   const gitPushOutput = await getExecOutput("git", [
