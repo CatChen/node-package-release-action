de0e1da9ee752b1fbf68d1b84c06cc20e4f61cad
Cat Chen <catchen@catchen.me>
2023-03-18 11:31:52 -0700

    yarn build && yarn package

--- dist/index.js
+++ dist/index.js
@@ -18011,7 +18011,7 @@ module.exports = Object.assign(index_js_1.default, { default: index_js_1.default
 
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.hasMagic = exports.Glob = exports.unescape = exports.escape = exports.globIterateSync = exports.globIterate = exports.glob = exports.globSync = exports.globStream = exports.globStreamSync = void 0;
+exports.hasMagic = exports.Glob = exports.unescape = exports.escape = exports.sync = exports.iterate = exports.iterateSync = exports.stream = exports.streamSync = exports.globIterate = exports.globIterateSync = exports.glob = exports.globSync = exports.globStream = exports.globStreamSync = void 0;
 const minimatch_1 = __nccwpck_require__(3355);
 const glob_js_1 = __nccwpck_require__(3707);
 const has_magic_js_1 = __nccwpck_require__(1945);
@@ -18031,14 +18031,25 @@ async function glob(pattern, options = {}) {
     return new glob_js_1.Glob(pattern, options).walk();
 }
 exports.glob = glob;
-function globIterate(pattern, options = {}) {
-    return new glob_js_1.Glob(pattern, options).iterate();
-}
-exports.globIterate = globIterate;
 function globIterateSync(pattern, options = {}) {
     return new glob_js_1.Glob(pattern, options).iterateSync();
 }
 exports.globIterateSync = globIterateSync;
+function globIterate(pattern, options = {}) {
+    return new glob_js_1.Glob(pattern, options).iterate();
+}
+exports.globIterate = globIterate;
+// aliases: glob.sync.stream() glob.stream.sync() glob.sync() etc
+exports.streamSync = globStreamSync;
+exports.stream = Object.assign(globStream, { sync: globStreamSync });
+exports.iterateSync = globIterateSync;
+exports.iterate = Object.assign(globIterate, {
+    sync: globIterateSync,
+});
+exports.sync = Object.assign(globSync, {
+    stream: globStreamSync,
+    iterate: globIterateSync,
+});
 /* c8 ignore start */
 var minimatch_2 = __nccwpck_require__(3355);
 Object.defineProperty(exports, "escape", ({ enumerable: true, get: function () { return minimatch_2.escape; } }));
@@ -18051,10 +18062,15 @@ Object.defineProperty(exports, "hasMagic", ({ enumerable: true, get: function ()
 exports["default"] = Object.assign(glob, {
     glob,
     globSync,
+    sync: exports.sync,
     globStream,
+    stream: exports.stream,
     globStreamSync,
+    streamSync: exports.streamSync,
     globIterate,
+    iterate: exports.iterate,
     globIterateSync,
+    iterateSync: exports.iterateSync,
     Glob: glob_js_1.Glob,
     hasMagic: has_magic_js_1.hasMagic,
     escape: minimatch_1.escape,
