9c94e773a5bd9c6c4198492fedd402b6d4d16924
Cat Chen <catchen@catchen.me>
2023-05-13 11:29:24 -0700

    Stopped checking exitCode because it getExecOutput should throw anyway

--- src/fetchEverything.ts
+++ src/fetchEverything.ts
@@ -1,32 +1,14 @@
-import { ExitCode } from '@actions/core';
 import { getExecOutput } from '@actions/exec';
 
 export async function fetchEverything() {
-  const gitFetchTagsOutput = await getExecOutput('git', [
-    'fetch',
-    '--tags',
-    'origin',
-  ]);
-  if (gitFetchTagsOutput.exitCode !== ExitCode.Success) {
-    throw new Error(gitFetchTagsOutput.stderr);
-  }
+  await getExecOutput('git', ['fetch', '--tags', 'origin']);
 
   const gitIsShallowRepositoryOutput = await getExecOutput('git', [
     'rev-parse',
     '--is-shallow-repository',
   ]);
-  if (gitIsShallowRepositoryOutput.exitCode !== ExitCode.Success) {
-    throw new Error(gitIsShallowRepositoryOutput.stderr);
-  }
 
   if (gitIsShallowRepositoryOutput.stdout.trim() === 'true') {
-    const gitFetchUnshallowOutput = await getExecOutput('git', [
-      'fetch',
-      '--unshallow',
-      'origin',
-    ]);
-    if (gitFetchUnshallowOutput.exitCode !== ExitCode.Success) {
-      throw new Error(gitFetchUnshallowOutput.stderr);
-    }
+    await getExecOutput('git', ['fetch', '--unshallow', 'origin']);
   }
 }
