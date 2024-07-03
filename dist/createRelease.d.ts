2563f5f935e5ee4294e1ed8e02873a73d2561c0c
Cat Chen <catchen@catchen.me>
2024-07-03 04:21:47 +0000

    Removed dist directory (#601)

--- dist/createRelease.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Octokit } from '@octokit/core';
-import type { Api } from '@octokit/plugin-rest-endpoint-methods/dist-types/types';
-export declare function createRelease(owner: string, repo: string, version: string, octokit: Octokit & Api): Promise<void>;
--- dist/getLastGitTag.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getLastGitTag(): Promise<string | null>;
