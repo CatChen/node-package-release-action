bdd46b7b2f1e21d779b6685161976c613088227d
Cat Chen <catchen@catchen.me>
2026-02-28 22:42:23 -0800

    Reduced noise from git tag

--- src/getAllGitTags.ts
+++ src/getAllGitTags.ts
@@ -2,7 +2,7 @@ import { getExecOutput } from '@actions/exec';
 import { valid } from 'semver';
 
 export async function getAllGitTags() {
-  const tagOutput = await getExecOutput('git', ['tag']);
+  const tagOutput = await getExecOutput('git', ['tag'], { silent: true });
 
   const allTags = tagOutput.stdout.split('\n');
   const versionTags = allTags.filter((tag) => valid(tag));
