4463731889a9d70595dc3b8f97dbde8b34410213
Cat Chen <catchen@catchen.me>
2026-03-14 23:45:20 -0700

    Fixed yarn warnings and migrated to @ianvs/prettier-plugin-sort-imports (#1004)

--- bundle/findLastSameReleaseTypeVersion.d.ts
+++ bundle/findLastSameReleaseTypeVersion.d.ts
@@ -1,2 +1,2 @@
-import { ReleaseType } from 'semver';
+import type { ReleaseType } from 'semver';
 export declare function findLastSameReleaseTypeVersion(releaseVersion: string, releaseType: ReleaseType): Promise<string | null>;
